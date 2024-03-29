5ca2f5115702a0aed90c06f9c06d29b0bb315a32
Rakesh Kumar Sahoo <152980437+RkSahooGit@users.noreply.github.com>
2024-03-29 12:21:29 +0530

    Add files via upload
    Adding some new stuffs.

---
+++ BMI--Calculator-main/BMICalculator-main/BMI CALCULATOR/BMI CALCULATOR/Models/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI CALCULATOR
+//
+//  Created by Rakesh Kumar Sahoo on 08/12/23.
+//
+
+import UIKit
+
+struct BMI {
+    
+    let value : Float
+    let adviceText : String
+    let colour : UIColor
+    
+}
